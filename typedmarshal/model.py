e95b586f73c167eafc0a48075d38cf99c37328f5
puhitaku <puhitaku@gmail.com>
2017-01-09 08:05:49 -0600

    More accurate Any/Optional/Union checks

--- typedmarshal/model.py
+++ typedmarshal/model.py
@@ -40,7 +40,7 @@ class MarshalModel:
             ins = obj()
         else:
             ins = obj
-        attrs = [a for a in ins.__class__.__dict__ if not a.startswith('_')]
+        attrs = [a for a, v in ins.__class__.__dict__.items() if not a.startswith('_') and not isinstance(v, type)]
         annot = get_type_hints(ins)
         attr_dict = OrderedDict((k, annot.get(k, None)) for k in attrs)
         attr_tree = cls._convert_annot_type_tree(attr_dict)
@@ -51,11 +51,14 @@ class MarshalModel:
         if isinstance(typ, type):
             return typ.__name__ in __builtins__
         else:
-            return typ.__class__.__name__ in __builtins__.__dict__
+            return typ.__class__.__name__ in __builtins__
 
     @staticmethod
     def _has_public_attribute(typ) -> bool:
-        return len([x for x in typ.__dict__ if not x.startswith('_')]) > 0
+        if isinstance(typ, type):
+            return len([x for x in typ.__dict__ if not x.startswith('_')]) > 0
+        else:
+            return len([x for x in typ.__class__.__dict__ if not x.startswith('_')]) > 0
 
     @staticmethod
     def _get_attribute_list(typ) -> List[str]:
@@ -66,6 +69,31 @@ class MarshalModel:
         """Identify attr type: builtin or derived from Generic class"""
         # return Generic in a.__mro__  # Derived from Generic
         return hasattr(typ, '__origin__')
+
+    @staticmethod
+    def _is_any_option_union(typ) -> bool:
+        """Identify attr type: Any, Union (Optional) or else"""
+        if hasattr(typ, '__origin__'):
+            return typ.__origin__ is Union or typ is Union
+        else:
+            return any(typ is t for t in [Any, Union, Optional])
+
+    @classmethod
+    def _get_root(cls, typ):
+        if cls._is_any_option_union(typ):
+            if hasattr(typ, '__origin__'):
+                if typ is Union:
+                    return Union
+                return typ.__origin__
+            else:
+                return typ
+        elif cls._is_generic(typ):
+            if typ.__origin__ is None:
+                return typ
+            else:
+                return typ.__origin__
+        else:
+            return typ
 
     @staticmethod
     def _has_nested_type(typ) -> bool:
@@ -94,11 +122,33 @@ class MarshalModel:
     def _unmarshal_with_type_check(self, attr, typ: Tuple[TypeLike]) -> Any:
         err = TypeCheckError('Type of value is different than annotated type.')
 
-        if self._is_generic(typ[0]):
-            if typ[0] is Any:
+        if self._is_any_option_union(typ[0]):
+            if self._get_root(typ[0]) is Any:
                 return attr
 
-            elif typ[0] is Dict:
+            elif self._get_root(typ[0]) is Union:
+                # This case is only for Optional type.
+                # De-serialized data structures don't preserve type definition,
+                # so unmarshaller doesn't check type strictly for now.
+
+                if any(not self._is_builtin(t) for t in typ[1:]):
+                    raise TypeCheckError('Unmarshaller does not support arbitrary Union. Primitive builtin types only.')
+
+                if attr is None:
+                    return None
+                else:
+                    if any(isinstance(attr, t) for t in typ[1:]):
+                        return attr
+                    else:
+                        raise err
+
+            elif self._get_root(typ[0]) is Optional:
+                # Plain Optional means there's no more type info.
+                # Unmarshaller checks nothing more.
+                return attr
+
+        elif self._is_generic(typ[0]):
+            if typ[0] is Dict:
                 if not isinstance(attr, dict):
                     raise err
 
@@ -110,30 +160,8 @@ class MarshalModel:
                     if not isinstance(k, typ[1]):
                         raise err
                     else:
-                        dict_items.append(self._unmarshal_with_type_check(v, typ[2]))
+                        dict_items[k] = self._unmarshal_with_type_check(v, typ[2])
                 return dict_items
-
-            elif typ[0] is Union:
-                # This case is only for Optional type.
-                # De-serialized data structures don't preserve type definition,
-                # so unmarshaller doesn't check type strictly for now.
-
-                if typ[2][0]() is not None:
-                    raise TypeCheckError('Unmarshaller does not support arbitrary Union.')
-
-                if attr is None:
-                    return None
-                else:
-                    return self._unmarshal_with_type_check(attr, typ[1])
-
-
-            elif typ[0] is Optional:
-                if attr is None:
-                    return None
-
-                if not len(typ) > 0:
-                    return attr
-                return self._unmarshal_with_type_check(attr, typ[1][0])
 
             elif typ[0] is List:
                 if not isinstance(attr, list):
@@ -175,7 +203,10 @@ class MarshalModel:
 
     def _unmarshal(self, target, target_attr_tree, item):
         for key, typ in target_attr_tree.items():
-            setattr(target, key, self._unmarshal_with_type_check(item.get(key, None), typ))
+            if typ is None:
+                setattr(target, key, item.get(key, None))
+            else:
+                setattr(target, key, self._unmarshal_with_type_check(item.get(key, None), typ))
 
     def load_json(self, json_in: Union[str, io.IOBase], lines=False, **kwargs):
         if isinstance(json_in, str):

ce09580788c404eaaa7a4a01e937e90fda918a3f
puhitaku <puhitaku@gmail.com>
2017-01-09 08:06:06 -0600

    Complete trello example

--- example/trello.py
+++ example/trello.py
@@ -1,33 +1,156 @@
 from typedmarshal import MarshalModel
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Any, Union
+
+
+class Prefs:
+    permissionLevel: str = ""
+    voting: str = ""
+    comments: str = ""
+    invitations: str = ""
+    selfJoin: bool = False
+    cardCovers: bool = False
+    cardAging: str = ""
+    calendarFeedEnabled: bool = False
+    background: str = ""
+    backgroundImage: Optional = None
+    backgroundImageScaled: Optional = None
+    backgroundTile: bool = False
+    backgroundBrightness: str = ""
+    backgroundColor: str = ""
+    canBePublic: bool = False
+    canBeOrg: bool = False
+    canBePrivate: bool = False
+    canInvite: bool = False
+
+class Membership:
+    id: str = ""
+    idMember: str = ""
+    memberType: str = ""
+    unconfirmed: bool = False
+    deactivated: bool = False
+
+class Label:
+    id: str = ""
+    idBoard: str = ""
+    name: str = ""
+    color: str = ""
+    uses: int = 0
+
+class Card:
+    class Badge:
+        votes: int = 0
+        viewingMemberVoted: bool = False
+        subscribed: bool = False
+        fogbugz: str = ""
+        checkItems: int = 0
+        checkItemsChecked: int = 0
+        comments: int = 0
+        attachments: int = 0
+        description: bool = False
+        due: Optional = None
+        dueComplete: bool = False
+
+    id = ""
+    checkItemStates: Optional = None
+    closed: bool = False
+    dateLastActivity: str = ""
+    desc: str = ""
+    descData: Optional = None
+    idBoard: str = ""
+    idList: str = ""
+    idMembersVoted: List = []
+    idShort: int = 0
+    idAttachmentCover: Optional = None
+    manualCoverAttachment: bool = False
+    idLabels: List = []
+    name: str = ""
+    pos: Union[int, float] = 0
+    shortLink: str = ""
+    badges: Badge = []
+    dueComplete: bool = False
+    due: Optional = None
+    email: str = ""
+    idChecklists: List = []
+    idMembers: List = []
+    labels: List = []
+    shortUrl: str = ""
+    subscribed: bool = False
+    url: str = ""
+    attachments: List = []
+    pluginData: List = []
+
+
+class Member:
+    id: str = ""
+    avatarHash: Optional = None
+    bio: str = ""
+    bioData: Optional[Dict] = None
+    confirmed: bool = False
+    fullName: str = ""
+    idPremOrgsAdmin: List = []
+    initials: str = ""
+    memberType: str = ""
+    products: List[int] = []
+    status: str = ""
+    url: str = ""
+    username: str = ""
+
+
+class TList:
+    id: str = ""
+    name: str = ""
+    closed: bool = False
+    idBoard: str = ""
+    pos: int = 0
+    subscribed: bool = False
+
+
+class Action:
+    class MemberCreator:
+        id: str = ""
+        avatarHash: Optional = None
+        fullName: str = ""
+        initials: str = ""
+        username: str = ""
+
+    id: str = ""
+    idMemberCreator: str = ""
+    data: Dict = dict()
+    type: str = ""
+    date: str = ""
+    memberCreator: MemberCreator = MemberCreator()
 
 
 class Trello(MarshalModel):
-    id: str = None
-    name: str = None
-    desc: str = None
-    descData = None
-    closed: bool = None
-    idOrganization = None
-    invited: bool = None
-    pinned: bool = None
-    starred: bool = None
-    url: str = None
-    prefs: Dict = None
-    invitations: List = None
-    memberships: List[Dict] = None
-    shortLink: str = None
-    subscribed: bool = None
-    labelNames: Dict = None
-    powerUps: List = None
-    dateLastActivity: str = None
-    dateLastView: str = None
-    shortUrl: str = None
-    idTags: List = None
-    checklists: List = None
-    pluginData: List = None
-    labels: List[Dict] = None
-    cards: List[Dict] = None
-    members: List[Dict] = None
-    lists: List[Dict] = None
-    actions: List[Dict] = None
+    id: str = ""
+    name: str = ""
+    desc: str = ""
+    descData: Optional = None
+    closed: bool = False
+    idOrganization: Optional = None
+    invited: bool = False
+    pinned: bool = False
+    starred: bool = False
+    url: str = ""
+    prefs: Prefs = None
+    invitations: List = []
+    memberships: List[Membership] = []
+    shortLink: str = ""
+    subscribed: bool = False
+    labelNames: Dict[str, str] = dict()
+    powerUps: List = []
+    dateLastActivity: str = ""
+    dateLastView: str = ""
+    shortUrl: str = ""
+    idTags: List = []
+    checklists: List = []
+    pluginData: List = []
+    labels: List[Label] = []
+    cards: List[Card] = []
+    members: List[Member] = []
+    lists: List[TList] = []
+    actions: List[Action] = []
+
+trello = Trello()
+trello.load_json(open('trello.json'))
+print()
